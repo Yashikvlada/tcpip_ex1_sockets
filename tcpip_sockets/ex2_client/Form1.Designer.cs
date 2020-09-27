38d49538c1aeff7dd3bfd09445a2efeff85f568e
yv <yashikvlada@gmail.com>
2020-09-28 02:05:43 +0300

    deleted practic1 and practic2 from sol

--- tcpip_sockets/ex2_client/Form1.Designer.cs
+++ tcpip_sockets/ex2_client/Form1.Designer.cs
@@ -44,18 +44,20 @@
             this.textBox_console.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox_console.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox_console.ForeColor = System.Drawing.Color.LightSteelBlue;
-            this.textBox_console.Location = new System.Drawing.Point(3, 43);
+            this.textBox_console.Location = new System.Drawing.Point(4, 67);
+            this.textBox_console.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox_console.Multiline = true;
             this.textBox_console.Name = "textBox_console";
             this.textBox_console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox_console.Size = new System.Drawing.Size(630, 251);
+            this.textBox_console.Size = new System.Drawing.Size(946, 366);
             this.textBox_console.TabIndex = 0;
             // 
             // button_connect
             // 
-            this.button_connect.Location = new System.Drawing.Point(3, 3);
+            this.button_connect.Location = new System.Drawing.Point(4, 5);
+            this.button_connect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button_connect.Name = "button_connect";
-            this.button_connect.Size = new System.Drawing.Size(104, 23);
+            this.button_connect.Size = new System.Drawing.Size(156, 35);
             this.button_connect.TabIndex = 1;
             this.button_connect.Text = "Connect";
             this.button_connect.UseVisualStyleBackColor = true;
@@ -63,17 +65,19 @@
             // 
             // textBox_port
             // 
-            this.textBox_port.Location = new System.Drawing.Point(305, 3);
+            this.textBox_port.Location = new System.Drawing.Point(453, 5);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox_port.Name = "textBox_port";
-            this.textBox_port.Size = new System.Drawing.Size(95, 20);
+            this.textBox_port.Size = new System.Drawing.Size(140, 26);
             this.textBox_port.TabIndex = 3;
             this.textBox_port.Text = "1024";
             // 
             // textBox_ip
             // 
-            this.textBox_ip.Location = new System.Drawing.Point(113, 3);
+            this.textBox_ip.Location = new System.Drawing.Point(168, 5);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox_ip.Name = "textBox_ip";
-            this.textBox_ip.Size = new System.Drawing.Size(186, 20);
+            this.textBox_ip.Size = new System.Drawing.Size(277, 26);
             this.textBox_ip.TabIndex = 2;
             this.textBox_ip.Text = "127.0.0.1";
             // 
@@ -85,11 +89,12 @@
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 62F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(636, 285);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(954, 438);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
             // tableLayoutPanel2
@@ -98,33 +103,36 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 340F));
             this.tableLayoutPanel2.Controls.Add(this.button_connect, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.textBox_port, 2, 0);
             this.tableLayoutPanel2.Controls.Add(this.textBox_ip, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.comboBox_query, 3, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 5);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(630, 34);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(946, 52);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // comboBox_query
             // 
             this.comboBox_query.FormattingEnabled = true;
-            this.comboBox_query.Location = new System.Drawing.Point(406, 3);
+            this.comboBox_query.Location = new System.Drawing.Point(601, 5);
+            this.comboBox_query.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox_query.Name = "comboBox_query";
-            this.comboBox_query.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_query.Size = new System.Drawing.Size(180, 28);
             this.comboBox_query.TabIndex = 4;
             // 
             // Form_client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 285);
+            this.ClientSize = new System.Drawing.Size(954, 438);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_client";
             this.Text = "Client";
             this.tableLayoutPanel1.ResumeLayout(false);
